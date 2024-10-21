5417f8bc77e1d8c02f6239cb6e8a774a94afb641
ziinspiration <ramddbgk@gmail.com>
2024-10-21 19:24:25 +0700

    Vidio 10 - oophp - Static Keyword

---
+++ oophp/Static Keyword/staticKeyword.php
@@ -0,0 +1,22 @@
+<?php
+class Contoh
+{
+    public static $angka = 1;
+
+    public function halo()
+    {
+        return "Halo " . self::$angka++ . " kali";
+    }
+}
+
+$obj = new Contoh();
+echo $obj->halo();
+echo "<br>";
+echo $obj->halo();
+echo "<br>";
+echo "<hr>";
+$obj2 = new Contoh();
+echo $obj2->halo();
+echo "<br>";
+echo $obj2->halo();
+echo "<br>";
