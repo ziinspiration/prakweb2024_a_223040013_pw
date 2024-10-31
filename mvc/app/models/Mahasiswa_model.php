ddce2632d9ba296ac84918bb1ca781e7ab36ee7e
ziinspiration <ramddbgk@gmail.com>
2024-11-01 01:18:04 +0700

    Vidio 8 - mvc - Insert Data

--- mvc/app/models/Mahasiswa_model.php
+++ mvc/app/models/Mahasiswa_model.php
@@ -21,4 +21,20 @@ class Mahasiswa_model
         $this->db->bind(':id', $id);
         return $this->db->single();
     }
+
+    public function tambahDataMahasiswa($data)
+    {
+        // Memperbaiki query untuk menambahkan mahasiswa
+        $query = "INSERT INTO mahasiswa (id, nama, nrp, email, jurusan) VALUES (NULL, :nama, :nrp, :email, :jurusan)";
+
+        $this->db->query($query);
+        $this->db->bind(':nama', $data['nama']);
+        $this->db->bind(':nrp', $data['nrp']);
+        $this->db->bind(':email', $data['email']);
+        $this->db->bind(':jurusan', $data['jurusan']);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
 }
--- mvc/app/views/mahasiswa/index.php
+++ mvc/app/views/mahasiswa/index.php
@@ -1,6 +1,10 @@
-<div class="container mt-5">
+<div class="container mt-3">
     <div class="row">
         <div class="col-6">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
+                Tambah Data Mahasiswa
+            </button>
+            <br><br>
             <h3>Daftar Mahasiswa</h3>
             <ul class="list-group">
                 <?php foreach ($data['mhs'] as $mhs) : ?>
@@ -11,6 +15,51 @@
                     </li>
                 <?php endforeach; ?>
             </ul>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="judulModal">Tambah Data Mahasiswa</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+
+                <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+                    <div class="mb-3">
+                        <label for="nama" class="form-label">Nama</label>
+                        <input type="text" class="form-control" id="nama" name="nama" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="nrp" class="form-label">NRP</label>
+                        <input type="number" class="form-control" id="nrp" name="nrp" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="jurusan">Jurusan</label>
+                        <select class="form-control" id="jurusan" name="jurusan">
+                            <option value="Teknik Informatika">Teknik Informatika</option>
+                            <option value="Teknik Mesin">Teknik Mesin</option>
+                            <option value="Teknik Industri">Teknik Industri</option>
+                            <option value="Teknik Pangan">Teknik Pangan</option>
+                            <option value="Teknik Planologi">Teknik Planologi</option>
+                            <option value="Teknik Lingkungan">Teknik Lingkungan</option>
+                        </select>
+                    </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Tambah Data</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
